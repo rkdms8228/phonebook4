336fd0cabbd11e9de42c963a92728c0f163ddc91
가은 <rkdms8228@naver.com>
2022-06-16 14:22:31 +0900

    SqlSession

--- src/main/java/com/javaex/dao/PhoneDao.java
+++ src/main/java/com/javaex/dao/PhoneDao.java
@@ -1,12 +1,6 @@
 package com.javaex.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
-
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,18 +14,6 @@ public class PhoneDao {
 	@Autowired
 	private SqlSession sqlSession;
 
-	@Autowired
-	private DataSource dataSource;
-
-	// 0. import java.sql.*;
-	private Connection conn = null;
-	private PreparedStatement pstmt = null;
-	private ResultSet rs = null;
-
-	private String driver = "oracle.jdbc.driver.OracleDriver";
-	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	private String id = "phonedb";
-	private String pw = "phonedb";
 
 	//리스트 불러오기
 	public List<PhoneVo> getPersonList() {
@@ -87,85 +69,5 @@ public class PhoneDao {
 		return phoneVo;
 		
 	}
-
-	private void getConnection() {
-		try {
-			// 1. JDBC 드라이버 (Oracle) 로딩
-			// Class.forName(driver);
-
-			// 2. Connection 얻어오기
-			// conn = DriverManager.getConnection(url, id, pw);
-			// System.out.println("접속성공");
-			conn = dataSource.getConnection();
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-	}
-
-	private void close() {
-		// 5. 자원정리
-		try {
-			if (rs != null) {
-				rs.close();
-			}
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-	}
-
-
-	// 1명 정보 가져오기
-	public PhoneVo getPerson2(int personId) {
-		PhoneVo phoneVo = null;
-
-		this.getConnection();
-
-		try {
-
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = "";
-			query += " select  person_id, ";
-			query += "         name, ";
-			query += "         hp, ";
-			query += "         company ";
-			query += " from person ";
-			query += " where person_id = ? ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, personId);
-
-			// 실행
-			rs = pstmt.executeQuery();
-
-			// 4.결과처리
-			while (rs.next()) {
-
-				int id = rs.getInt("person_id");
-				String name = rs.getString("name");
-				String hp = rs.getString("hp");
-				String company = rs.getString("company");
-
-				phoneVo = new PhoneVo(id, name, hp, company);
-			}
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-
-		this.close();
-
-		return phoneVo;
-	}
-
-
 
 }
